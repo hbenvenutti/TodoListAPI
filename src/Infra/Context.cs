8a051a76d10f76d6578a5a60cea14977903feabc
Huam Benvenutti <huambenvenutti@protonmail.com>
2023-07-31 17:19:45 -0300

    fix: corrigido o nome de Task para TaskModel

--- src/Infra/Context.cs
+++ src/Infra/Context.cs
@@ -4,7 +4,7 @@ namespace TodoList.Infra;
 
 public class Context : DbContext
 {
-    public DbSet<Task> Tasks { get; set; }
+    public DbSet<TaskModel> Tasks { get; set; }
 
     public Context(DbContextOptions<Context> options) : base(options) {}
 }
